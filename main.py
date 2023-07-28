93498c16de6aa50e4156f7dcbfc0be01cd25e534
SoumyaCodes2020 <skstamu@outlook.com>
2023-07-28 18:46:26 +0100

    Used RevChatGPT to create responses [UNTESTED]

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
 from flask import Flask, request
+from dbManager import DBManager
+from revChatGPT.V1 import Chatbot
 
 
 # Load Twilio config from .env
@@ -14,6 +16,9 @@ AUTH_TOKEN = os.getenv('AUTHTOKEN')
 # Init Twilio Client and Flask
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 app = Flask(__name__)
+
+# Get an instance of DbManager
+dbManager = DBManager()
 
 # Now we set up Flask
 
@@ -41,7 +46,43 @@ def reply():
             from_='whatsapp:+14155238886',
             to=fromnum
         )
+        return
+    elif msg.strip().lower() == '!start':
+        # All we need is a new conversation ID to restart the conversation
+        dbManager.remove_conversation_id(fromnum)
+        dbManager.create_new_row(fromnum)
+
+        message = client.messages.create(
+            body='Alright! A New Conversation has been started for you!',
+            from_='whatsapp:+14155238886',
+            to=fromnum
+        )
+        return
+
+    elif msg.strip().lower() == '!help':
+        message = client.messages.create(
+            body="Don't Fret! I'm here to help! I am a Whatsapp bot that uses ChatGPT! To restart a conversation, simply type: '!start'. And that's about it!",
+            from_='whatsapp:+14155238886',
+            to=fromnum
+        )
+        return
+
+    # Now we can use RevChatGPT to create a conversation
+    chatbot = Chatbot(config={
+        "email": "example@example.com",
+        "password": "password1234"
+    })
+
+    response = ""
+    for data in chatbot.ask(prompt):
+        response = data["message"]
+
+    message = client.messages.create(
+        body=response,
+        from_='whatsapp:+14155238886',
+        to=fromnum
+    )
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Change this to False
+    app.run(debug=True)
